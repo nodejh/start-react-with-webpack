0497a99391f39d032386aedfd600c9840579522f
nodejh <jianghangscu@gmail.com>
2016-11-19 16:17:13 +0800

    修改webpack.config.js配置

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,30 @@
-var webpack = require('webpack');
-var path = require('path');
+const webpack = require('webpack');
+const path = require('path');
 
-var BUILD_DIR = path.resolve(__dirname, './build');
-var APP_DIR = path.resolve(__dirname, './app');
+// 定义打包目录路径
+const BUILD_DIR = path.resolve(__dirname, './build');
+// 定义组件目录路径
+const APP_DIR = path.resolve(__dirname, './app');
 
-var config = {
-  entry: [
-    'webpack-dev-server/client?http://localhost:8080',
-    'webpack/hot/dev-server',
-    APP_DIR + '/index.jsx'
-  ],
+const config = {
+  entry: `${APP_DIR}/index.jsx`, // 文件打包的入口点
   output: {
-    path: BUILD_DIR,
-    filename: 'bundle.js'
+    path: BUILD_DIR, // 输出目录的绝对路径
+    filename: 'bundle.js', // 输出的每个包的相对路径
   },
-  module : {
-    loaders : [
-      {
-        test : /\.jsx?/,
-        include : APP_DIR,
-        loader : 'babel'
-      }
-    ]
-  },
-  // Important! Do not remove ''. If you do, imports without
-  // an extension won't work anymore!
   resolve: {
-    extensions: ['', '.js', '.jsx']
-  }
+    extensions: ['', '.js', '.jsx'], // 开启后缀名的自动补全
+  },
+  module: {
+    loaders: [{
+      test: /\.(js|jsx)$/,
+      exclude: /(node_modules|bower_components)/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['es2015', 'react'],
+      }
+    }]
+  },
 };
-
 
 module.exports = config;
